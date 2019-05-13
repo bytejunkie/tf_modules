afa0dcb6a8dd9d29dc404ab132c51b90664019e4
Matt Short <mnshort@aqa.org.uk>
2019-05-13 13:43:41 +0100

    simpler tags

--- resource_group/resource_group.tf
+++ resource_group/resource_group.tf
@@ -2,9 +2,4 @@ resource "azurerm_resource_group" "resource_group" {
   name     = "${var.resource_group_name}"
   location = "North Europe"
 
-  tags = {
-    ${element(keys(var.rg_tags), count.index)} =     ${element(values(var.rg_tags), count.index)} 
-
-    count = ${length(keys(var.rg_tags))}
-  }
-}
+  tags = "${var.rg_tags}"
