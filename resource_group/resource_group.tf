42a997570b6e01f3aaecbe7cadaa4247bd7192a2
Matthew Short <mattshort77@live.com>
2020-05-03 13:49:01 +0000

    v12 ammendments

--- resource_group/resource_group.tf
+++ resource_group/resource_group.tf
@@ -1,9 +1,9 @@
 resource "azurerm_resource_group" "resource_group" {
-  name     = "${element(var.resource_group_names, count.index)}"
+  name     = element(var.resource_group_names, count.index)
   location = "North Europe"
 
   tags = var.tags
 
   count = length(var.resource_group_names)
-  
+
 }
