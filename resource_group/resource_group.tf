6a8953bb62eefb744d243f18c43a5ab3fbbabf2f
Matthew Short <mattshort77@live.com>
2020-05-03 13:44:37 +0000

    getting these ready for v12

--- resource_group/resource_group.tf
+++ resource_group/resource_group.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "resource_group" {
   name     = "${element(var.resource_group_names, count.index)}"
   location = "North Europe"
 
-  tags = "${var.tags}"
+  tags = var.tags
 
-  count = "${length(var.resource_group_names)}"
+  count = length(var.resource_group_names)
 }
--- resource_group/variables.tf
+++ resource_group/variables.tf
@@ -1,11 +1,9 @@
 variable "resource_group_names" {
     description = "resource group name"
-    type = "list"
     default = []
 }
 
 variable "tags" {
     description = "tags to apply to resource group"
-    type = "map"
     default = {}
 }
